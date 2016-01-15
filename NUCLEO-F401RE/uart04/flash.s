861a6ff92eff79ef2c34f53ebbf34364e2317b7d
dwelch <dwelch@dwelch.com>
2016-01-14 23:37:29 -0500

    adding uart04 example, uses pll to get to 16Mhz.

--- NUCLEO-F401RE/uart04/flash.s
+++ NUCLEO-F401RE/uart04/flash.s
@@ -3,29 +3,40 @@
 .cpu cortex-m0
 .thumb
 ;@-----------------------
+
 .thumb_func
 .global _start
 _start:
-    ldr r0,stacktop
-    mov sp,r0
+stacktop: .word 0x20001000
+.word reset
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+.word hang
+
+.thumb_func
+reset:
     bl notmain
     b hang
 .thumb_func
 hang:   b .
 ;@-----------------------
 .align
-stacktop: .word 0x20001000
 ;@-----------------------
 .thumb_func
 .globl PUT16
 PUT16:
     strh r1,[r0]
-    bx lr
-;@-----------------------
-.thumb_func
-.globl GET16
-GET16:
-    ldrh r0,[r0]
     bx lr
 ;@-----------------------
 .thumb_func
@@ -38,6 +49,12 @@ PUT32:
 .globl GET32
 GET32:
     ldr r0,[r0]
+    bx lr
+;@-----------------------
+.thumb_func
+.globl GET16
+GET16:
+    ldrh r0,[r0]
     bx lr
 ;@-----------------------
 .thumb_func
---
+++ NUCLEO-F411RE/uart04/uart04.c
@@ -0,0 +1,283 @@
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+
+#define BRICK_BUTTON
+
+void PUT16 ( unsigned int, unsigned int );
+void PUT32 ( unsigned int, unsigned int );
+unsigned int GET16 ( unsigned int );
+unsigned int GET32 ( unsigned int );
+void dummy ( unsigned int );
+
+#define RCCBASE 0x40023800
+#define RCC_CR          (RCCBASE+0x00)
+#define RCC_PLLCFGR     (RCCBASE+0x04)
+#define RCC_CFGR        (RCCBASE+0x08)
+#define RCC_APB1RSTR    (RCCBASE+0x20)
+#define RCC_AHB1ENR     (RCCBASE+0x30)
+#define RCC_APB1ENR     (RCCBASE+0x40)
+#define RCC_BDCR        (RCCBASE+0x70)
+
+#define GPIOABASE 0x40020000
+#define GPIOA_MODER     (GPIOABASE+0x00)
+#define GPIOA_OTYPER    (GPIOABASE+0x04)
+#define GPIOA_OSPEEDR   (GPIOABASE+0x08)
+#define GPIOA_PUPDR     (GPIOABASE+0x0C)
+#define GPIOA_BSRR      (GPIOABASE+0x18)
+#define GPIOA_AFRL      (GPIOABASE+0x20)
+
+#define GPIOCBASE 0x40020800
+#define GPIOC_MODER     (GPIOCBASE+0x00)
+#define GPIOC_OTYPER    (GPIOCBASE+0x04)
+#define GPIOC_OSPEEDR   (GPIOCBASE+0x08)
+#define GPIOC_PUPDR     (GPIOCBASE+0x0C)
+#define GPIOC_IDR       (GPIOCBASE+0x10)
+
+#define USART2BASE 0x40004400
+#define USART2_SR       (USART2BASE+0x00)
+#define USART2_DR       (USART2BASE+0x04)
+#define USART2_BRR      (USART2BASE+0x08)
+#define USART2_CR1      (USART2BASE+0x0C)
+
+//PA2 is USART2_TX alternate function 1
+//PA3 is USART2_RX alternate function 1
+
+//------------------------------------------------------------------------
+#ifdef BRICK_BUTTON
+//------------------------------------------------------------------------
+static void button_init ( void )
+{
+    unsigned int ra;
+
+    ra=GET32(RCC_AHB1ENR);
+    ra|=1<<2; //enable GPIOC
+    PUT32(RCC_AHB1ENR,ra);
+
+    //moder 2b00
+    ra=GET32(GPIOC_MODER);
+    ra&=~(3<<26); //PC13
+    PUT32(GPIOC_MODER,ra);
+
+    //pupdr 2b00
+    ra=GET32(GPIOC_PUPDR);
+    ra&=~(3<<26); //PC13
+    PUT32(GPIOC_PUPDR,ra);
+}
+//------------------------------------------------------------------------
+static unsigned int read_button ( void )
+{
+    return(GET32(GPIOC_IDR)&(1<<13));
+}
+//------------------------------------------------------------------------
+static void led_init ( void )
+{
+    unsigned int ra;
+
+    ra=GET32(RCC_AHB1ENR);
+    ra|=1<<0; //enable GPIOA
+    PUT32(RCC_AHB1ENR,ra);
+
+    ra=GET32(GPIOA_MODER);
+    ra&=~(3<<10); //PA5
+    ra|=1<<10; //PA5
+    PUT32(GPIOA_MODER,ra);
+
+    ra=GET32(GPIOA_OTYPER);
+    ra&=~(1<<5); //PA5
+    PUT32(GPIOA_OTYPER,ra);
+
+    ra=GET32(GPIOA_OSPEEDR);
+    ra|=3<<10; //PA5
+    PUT32(GPIOA_OSPEEDR,ra);
+    //pupdr
+    ra=GET32(GPIOA_PUPDR);
+    ra&=~(3<<10); //PA5
+    PUT32(GPIOA_PUPDR,ra);
+}
+//------------------------------------------------------------------------
+static void led_on ( void )
+{
+    PUT32(GPIOA_BSRR,((1<<5)<<0));
+}
+//------------------------------------------------------------------------
+static void led_off ( void )
+{
+    PUT32(GPIOA_BSRR,((1<<5)<<16));
+}
+//------------------------------------------------------------------------
+#endif //BRICK_BUTTON
+//------------------------------------------------------------------------
+static int clock_init ( void )
+{
+    unsigned int ra;
+
+#ifdef BRICK_BUTTON
+    //this is a safety measure, if the code below that messes with
+    //the clock source, bricks the microcontroller then press and
+    //hold the user button, and press reset, on that reset this code
+    //will see the button pressed and go into this infinite loop
+    //instead of into the code below, allowing us to reprogram the
+    //microcontrollers flash with hopefully a not buggy program.
+    button_init();
+    if(read_button()==0)
+    {
+        //make the led glow
+        led_init();
+        while(1)
+        {
+            led_on();
+            led_off();
+        }
+    }
+    //as delivered this code does not brick the microcontroller
+    //the above was for me to develop this code and for you to change
+    //this code as desired.
+#endif //BRICK_BUTTON
+
+    //switch to external clock.
+    ra=GET32(RCC_CR);
+    ra|=1<<16;
+    PUT32(RCC_CR,ra);
+    while(1) if(GET32(RCC_CR)&(1<<17)) break;
+    if(1)
+    {
+        ra=GET32(RCC_CFGR);
+        ra&=~3;
+        ra|=1;
+        PUT32(RCC_CFGR,ra);
+        while(1) if(((GET32(RCC_CFGR)>>2)&3)==1) break;
+    }
+    //HSE ready
+    //PLLM aim for 2mhz so 8/2=4
+    //PLLN input is 2, want >=100 and <=432 so between 50 and 216
+    //PLLP  16Mhz*8 = 128, 16MHz*6 = 96, not enough
+    //so PLLP is 8 VCO 128 so PLLN is 64
+    //dont really care about PLLQ but have to have something so 8
+    PUT32(RCC_PLLCFGR,0x20000000|(8<<24)|(1<<22)|(3<<16)|(64<<6)|(4<<0));
+    ra=GET32(RCC_CR);
+    ra|=1<<24;
+    PUT32(RCC_CR,ra);
+    while(1) if(GET32(RCC_CR)&(1<<25)) break;
+    ra=GET32(RCC_CFGR);
+    ra&=~3;
+    ra|=2;
+    PUT32(RCC_CFGR,ra);
+    while(1) if(((GET32(RCC_CFGR)>>2)&3)==2) break;
+
+    return(0);
+}
+//------------------------------------------------------------------------
+int uart2_init ( void )
+{
+    unsigned int ra;
+
+    ra=GET32(RCC_AHB1ENR);
+    ra|=1<<0; //enable port A
+    PUT32(RCC_AHB1ENR,ra);
+
+    ra=GET32(RCC_APB1ENR);
+    ra|=1<<17; //enable USART2
+    PUT32(RCC_APB1ENR,ra);
+
+    ra=GET32(GPIOA_MODER);
+    ra&=~(3<<4); //PA2
+    ra&=~(3<<6); //PA3
+    ra|=2<<4; //PA2
+    ra|=2<<6; //PA3
+    PUT32(GPIOA_MODER,ra);
+
+    ra=GET32(GPIOA_OTYPER);
+    ra&=~(1<<2); //PA2
+    ra&=~(1<<3); //PA3
+    PUT32(GPIOA_OTYPER,ra);
+
+    ra=GET32(GPIOA_OSPEEDR);
+    ra|=3<<4; //PA2
+    ra|=3<<6; //PA3
+    PUT32(GPIOA_OSPEEDR,ra);
+
+    ra=GET32(GPIOA_PUPDR);
+    ra&=~(3<<4); //PA2
+    ra&=~(3<<6); //PA3
+    PUT32(GPIOA_PUPDR,ra);
+
+    ra=GET32(GPIOA_AFRL);
+    ra&=~(0xF<<8); //PA2
+    ra&=~(0xF<<12); //PA3
+    ra|=0x7<<8; //PA2
+    ra|=0x7<<12; //PA3
+    PUT32(GPIOA_AFRL,ra);
+
+    ra=GET32(RCC_APB1RSTR);
+    ra|=1<<17; //reset USART2
+    PUT32(RCC_APB1RSTR,ra);
+    ra&=~(1<<17);
+    PUT32(RCC_APB1RSTR,ra);
+
+    //PUT32(USART2_CR1,(1<<13));
+
+    //16000000/(16*115200) = 8.68  8+10/16ths or do you round that to 11?
+    PUT32(USART2_BRR,0x8A);
+//    PUT32(USART2_BRR,0x45);
+    PUT32(USART2_CR1,(1<<3)|(1<<2)|(1<<13));
+
+    return(0);
+}
+//------------------------------------------------------------------------
+void uart2_send ( unsigned int x )
+{
+    while(1) if(GET32(USART2_SR)&(1<<7)) break;
+    PUT32(USART2_DR,x);
+}
+//------------------------------------------------------------------------
+unsigned int uart2_recv ( void )
+{
+    while(1) if((GET32(USART2_SR))&(1<<5)) break;
+    return(GET32(USART2_DR));
+}
+//------------------------------------------------------------------------
+void hexstrings ( unsigned int d )
+{
+    //unsigned int ra;
+    unsigned int rb;
+    unsigned int rc;
+
+    rb=32;
+    while(1)
+    {
+        rb-=4;
+        rc=(d>>rb)&0xF;
+        if(rc>9) rc+=0x37; else rc+=0x30;
+        uart2_send(rc);
+        if(rb==0) break;
+    }
+    uart2_send(0x20);
+}
+//------------------------------------------------------------------------
+void hexstring ( unsigned int d )
+{
+    hexstrings(d);
+    uart2_send(0x0D);
+    uart2_send(0x0A);
+}
+//------------------------------------------------------------------------
+int notmain ( void )
+{
+    //unsigned int ra;
+    unsigned int rx;
+
+    clock_init();
+    uart2_init();
+    hexstring(0x12345678);
+
+    while(1)
+    {
+        rx=uart2_recv();
+        if(rx==0x0D) uart2_send(0x0A);
+        uart2_send(rx);
+    }
+    return(0);
+}
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
