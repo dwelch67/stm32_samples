f919e1a168cf05f4c48280b94c0a22424d3b87cd
dwelch <dwelch>
2016-05-09 16:50:12 -0400

    adding blinker06 example

---
+++ DWSTM32/blinker06/blinker06.c
@@ -0,0 +1,156 @@
+
+void PUT32 ( unsigned int, unsigned int );
+unsigned int GET32 ( unsigned int );
+void PUT16 ( unsigned int, unsigned int );
+unsigned int GET16 ( unsigned int );
+void dummy ( unsigned int );
+
+#define GPIOABASE 0x48000000
+#define RCCBASE 0x40021000
+
+#define TIMER14BASE 0x40002000
+
+
+int notmain ( void )
+{
+    unsigned int ra;
+    unsigned int rx;
+
+    ra=GET32(RCCBASE+0x14);
+    ra|=1<<17; //enable port a
+    PUT32(RCCBASE+0x14,ra);
+    //moder
+    ra=GET32(GPIOABASE+0x00);
+    ra&=~(3<<8); //PA4
+    ra|=(2<<8); //PA4 alternate function
+    PUT32(GPIOABASE+0x00,ra);
+    //AFRL
+    ra=GET32(GPIOABASE+0x20);
+    ra&=~(0xF<<16); //PA4
+    ra|=4<<16; //PA4
+    PUT32(GPIOABASE+0x20,ra);
+
+    ra=GET32(RCCBASE+0x1c);
+    ra|=1<<8; //enable timer 14
+    PUT32(RCCBASE+0x1C,ra);
+
+
+if(1)
+{
+    
+    PUT16(TIMER14BASE+0x00,0x0000);
+    PUT16(TIMER14BASE+0x2C,0xFFFF);
+    PUT16(TIMER14BASE+0x24,0x0000);
+    PUT16(TIMER14BASE+0x28,0x003F);
+    //PUT16(TIMER14BASE+0x18,0x0030);
+    PUT16(TIMER14BASE+0x20,0x0001);
+    //PUT16(TIMER14BASE+0x34,0x0000);
+    PUT16(TIMER14BASE+0x00,0x0001);
+
+    for(rx=0;rx<4;rx++)
+    {
+        PUT16(TIMER14BASE+0x18,0x0050);
+        while(1)
+        {
+            ra=GET16(TIMER14BASE+0x24);
+            if((ra&(1<<15))!=0) break;
+        }
+        PUT16(TIMER14BASE+0x18,0x0040);
+        while(1)
+        {
+            ra=GET16(TIMER14BASE+0x24);
+            if((ra&(1<<15))==0) break;
+        }
+    }
+}
+
+
+
+if(1)
+{
+    
+    PUT16(TIMER14BASE+0x00,0x0000);
+    PUT16(TIMER14BASE+0x2C,0xFFFF);
+    PUT16(TIMER14BASE+0x24,0x0000);
+    PUT16(TIMER14BASE+0x28,0x003F);
+    PUT16(TIMER14BASE+0x18,0x0030);
+    PUT16(TIMER14BASE+0x20,0x0001);
+    //PUT16(TIMER14BASE+0x34,0x0000);
+    PUT16(TIMER14BASE+0x00,0x0001);
+
+    for(rx=0;rx<6;rx++)
+    {
+        while(1)
+        {
+            ra=GET16(TIMER14BASE+0x10);
+            if(ra)
+            {
+                PUT16(TIMER14BASE+0x10,0);
+                break;
+            }
+        }
+    }
+
+}
+
+
+if(1)
+{
+    PUT16(TIMER14BASE+0x00,0x0000);
+    PUT16(TIMER14BASE+0x2C,0x7FFF);
+    PUT16(TIMER14BASE+0x24,0x0000);
+    PUT16(TIMER14BASE+0x28,0x003F);
+    PUT16(TIMER14BASE+0x18,0x0030);
+    PUT16(TIMER14BASE+0x20,0x0001);
+    //PUT16(TIMER14BASE+0x34,0x0000);
+    PUT16(TIMER14BASE+0x00,0x0001);
+
+    for(rx=0;rx<6;rx++)
+    {
+        while(1)
+        {
+            ra=GET16(TIMER14BASE+0x10);
+            if(ra)
+            {
+                PUT16(TIMER14BASE+0x10,0);
+                break;
+            }
+        }
+    }
+}
+
+
+
+if(1)
+{
+    PUT16(TIMER14BASE+0x00,0x0000);
+    PUT16(TIMER14BASE+0x2C,0x7FFF);
+    PUT16(TIMER14BASE+0x24,0x0000);
+    PUT16(TIMER14BASE+0x28,0x003F);
+    PUT16(TIMER14BASE+0x18,0x0030);
+    PUT16(TIMER14BASE+0x20,0x0001);
+    PUT16(TIMER14BASE+0x34,0x8000);
+    PUT16(TIMER14BASE+0x00,0x0001);
+
+    for(rx=0;rx<6;rx++)
+    {
+        while(1)
+        {
+            ra=GET16(TIMER14BASE+0x10);
+            if(ra)
+            {
+                PUT16(TIMER14BASE+0x10,0);
+                break;
+            }
+        }
+    }
+}
+
+
+
+    //PUT16(TIMER14BASE+0x00,0x0000);
+
+
+    
+    return(0);
+}
---
+++ DWSTM32/blinker06/sram.s
@@ -0,0 +1,49 @@
+
+;@-----------------------
+.cpu cortex-m0
+.thumb
+;@-----------------------
+.thumb_func
+.global _start
+_start:
+    ldr r0,stacktop
+    mov sp,r0
+    bl notmain
+    b hang
+.thumb_func
+hang:   b .
+;@-----------------------
+.align
+stacktop: .word 0x20001000
+;@-----------------------
+.thumb_func
+.globl PUT16
+PUT16:
+    strh r1,[r0]
+    bx lr
+;@-----------------------
+.thumb_func
+.globl PUT32
+PUT32:
+    str r1,[r0]
+    bx lr
+;@-----------------------
+.thumb_func
+.globl GET16
+GET16:
+    ldrh r0,[r0]
+    bx lr
+
+.thumb_func
+.globl GET32
+GET32:
+    ldr r0,[r0]
+    bx lr
+;@-----------------------
+.thumb_func
+.globl dummy
+dummy:
+    bx lr
+;@-----------------------
+.end
+;@-----------------------
