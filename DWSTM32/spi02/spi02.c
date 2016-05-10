5bf11751e70b89d0ed9e0f522c2e3423067834b3
dwelch <dwelch@dwelch.com>
2016-05-10 01:16:03 -0400

    adding two hardware spi examples

---
+++ DWSTM32/spi02/spi02.c
@@ -0,0 +1,88 @@
+
+void PUT32 ( unsigned int, unsigned int );
+unsigned int GET32 ( unsigned int );
+void PUT16 ( unsigned int, unsigned int );
+unsigned int GET16 ( unsigned int );
+void PUT8 ( unsigned int, unsigned int );
+void dummy ( unsigned int );
+
+#define GPIOABASE 0x48000000
+#define RCCBASE 0x40021000
+
+#define SPI1BASE 0x40013000
+
+int notmain ( void )
+{
+    unsigned int ra;
+
+//PA4 AF0 SPI1_NSS
+//PA5 AF0 SPI1_SCK
+//PA6 AF0 SPI1_MISO
+//PA7 AF0 SPI1_MOSI
+
+    ra=GET32(RCCBASE+0x14);
+    ra|=1<<17; //enable port a
+    PUT32(RCCBASE+0x14,ra);
+    //moder
+    ra=GET32(GPIOABASE+0x00);
+    ra&=~(3<<8); //PA4
+    ra|=(2<<8); //PA4 alternate function
+    ra&=~(3<<10); //PA5
+    ra|=(2<<10); //PA5 alternate function
+    ra&=~(3<<12); //PA6
+    ra|=(2<<12); //PA6 alternate function
+    ra&=~(3<<14); //PA7
+    ra|=(2<<14); //PA7 alternate function
+    PUT32(GPIOABASE+0x00,ra);
+    //AFRL
+    ra=GET32(GPIOABASE+0x20);
+    ra&=~(0xF<<16); //PA4
+    ra|=0<<16; //PA4
+    ra&=~(0xF<<20); //PA5
+    ra|=0<<20; //PA5
+    ra&=~(0xF<<24); //PA6
+    ra|=0<<24; //PA6
+    ra&=~(0xF<<28); //PA7
+    ra|=0<<28; //PA7
+    PUT32(GPIOABASE+0x20,ra);
+
+
+    //moder
+    ra=GET32(GPIOABASE+0x00);
+    ra&=~(3<<6); //PA3
+    ra|=1<<6; //PA3
+    PUT32(GPIOABASE+0x00,ra);
+    //OTYPER
+    ra=GET32(GPIOABASE+0x04);
+    ra&=~(1<<3); //PA3
+    PUT32(GPIOABASE+0x04,ra);
+    //ospeedr
+    ra=GET32(GPIOABASE+0x08);
+    ra|=3<<6; //PA3
+    PUT32(GPIOABASE+0x08,ra);
+    //pupdr
+    ra=GET32(GPIOABASE+0x0C);
+    ra&=~(3<<6); //PA3
+    PUT32(GPIOABASE+0x0C,ra);
+
+
+    ra=GET32(RCCBASE+0x18);
+    ra|=1<<12; //enable spi1
+    PUT32(RCCBASE+0x18,ra);
+
+    PUT32(RCCBASE+0x04,(0xF<<4)|(7<<8)); //SLOOOWWWW
+
+    PUT16(SPI1BASE+0x00,(0<<6)|(7<<3)|(1<<2)|0);
+    PUT16(SPI1BASE+0x04,0x070C);
+    PUT16(SPI1BASE+0x00,(1<<6)|(7<<3)|(1<<2)|0);
+
+    PUT32(GPIOABASE+0x18,(1<<(3+0)));
+    PUT8(SPI1BASE+0x0C,0xf0);
+    while(1)
+    {
+        if((GET16(SPI1BASE+0x08)&(1<<7))==0) break;
+    }
+    PUT32(GPIOABASE+0x18,(1<<(3+16)));
+
+    return(0);
+}
