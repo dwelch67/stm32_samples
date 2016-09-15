3e3014cdac2215dda7ee8ed1352ba810a45e4663
root <dwelch@dwelch.com>
2016-09-15 12:58:24 -0400

    adding nokia example using hw spi instead of bit banged

---
+++ NUCLEO-F031K6/spi01_nokia_bb/spi01.c
@@ -0,0 +1,160 @@
+
+void PUT32 ( unsigned int, unsigned int );
+unsigned int GET32 ( unsigned int );
+void dummy ( unsigned int );
+
+#define GPIOABASE 0x48000000
+#define RCCBASE 0x40021000
+
+//PA7 DIN
+//PA5 CLK
+//PA4 CE
+//PA3 D/C
+
+#define SET_MOSI    (1<<(7+0))
+#define SET_CLK     (1<<(5+0))
+#define SET_CS      (1<<(4+0))
+#define SET_DC      (1<<(3+0))
+#define RESET_MOSI  (1<<(7+16))
+#define RESET_CLK   (1<<(5+16))
+#define RESET_CS    (1<<(4+16))
+#define RESET_DC    (1<<(3+16))
+
+static void spi_delay ( void )
+{
+    unsigned int ra;
+    for(ra=0;ra<5;ra++) dummy(ra);
+}
+
+static void spi_dc ( unsigned int x )
+{
+    if(x) PUT32(GPIOABASE+0x18,SET_DC);
+    else  PUT32(GPIOABASE+0x18,RESET_DC);
+}
+static void spi_cs ( unsigned int x )
+{
+    if(x) PUT32(GPIOABASE+0x18,SET_CS);
+    else  PUT32(GPIOABASE+0x18,RESET_CS);
+}
+static void spi_clk ( unsigned int x )
+{
+    if(x) PUT32(GPIOABASE+0x18,SET_CLK);
+    else  PUT32(GPIOABASE+0x18,RESET_CLK);
+}
+static void spi_mosi ( unsigned int x )
+{
+    if(x) PUT32(GPIOABASE+0x18,SET_MOSI);
+    else  PUT32(GPIOABASE+0x18,RESET_MOSI);
+}
+
+static void spi_command ( unsigned int cmd )
+{
+    unsigned int ra;
+    unsigned int rb;
+
+    spi_dc(0);
+    spi_cs(0);
+    spi_delay();
+    for(rb=cmd,ra=0;ra<8;ra++,rb<<=1)
+    {
+        spi_mosi((rb>>7)&1);
+        spi_delay();
+        spi_clk(1);
+        spi_delay();
+        spi_clk(0);
+        spi_delay();
+    }
+    spi_cs(1);
+}
+
+static void spi_data ( unsigned int data )
+{
+    unsigned int ra;
+    unsigned int rb;
+
+    spi_dc(1);
+    spi_cs(0);
+    spi_delay();
+    for(rb=data,ra=0;ra<8;ra++,rb<<=1)
+    {
+        spi_mosi((rb>>7)&1);
+        spi_delay();
+        spi_clk(1);
+        spi_delay();
+        spi_clk(0);
+        spi_delay();
+    }
+    spi_cs(1);
+}
+
+int notmain ( void )
+{
+    unsigned int ra;
+    unsigned int rx;
+    
+//PA7 DIN
+//PA5 CLK
+//PA4 CE
+//PA3 D/C
+
+    ra=GET32(RCCBASE+0x14);
+    ra|=1<<17; //enable port a
+    PUT32(RCCBASE+0x14,ra);
+    //moder
+    ra=GET32(GPIOABASE+0x00);
+    ra&=~(3<<14); //PA7
+    ra&=~(3<<10); //PA5
+    ra&=~(3<<8); //PA4
+    ra&=~(3<<6); //PA3
+    ra|=1<<14; //PA7
+    ra|=1<<10; //PA5
+    ra|=1<<8; //PA4
+    ra|=1<<6; //PA3
+    PUT32(GPIOABASE+0x00,ra);
+    //OTYPER
+    ra=GET32(GPIOABASE+0x04);
+    ra&=~(1<<7); //PA7
+    ra&=~(1<<5); //PA5
+    ra&=~(1<<4); //PA4
+    ra&=~(1<<3); //PA3
+    PUT32(GPIOABASE+0x04,ra);
+    //ospeedr
+    ra=GET32(GPIOABASE+0x08);
+    ra|=3<<14; //PA7
+    ra|=3<<10; //PA5
+    ra|=3<<8; //PA4
+    ra|=3<<6; //PA3
+    PUT32(GPIOABASE+0x08,ra);
+    //pupdr
+    ra=GET32(GPIOABASE+0x0C);
+    ra&=~(3<<14); //PA7
+    ra&=~(3<<10); //PA5
+    ra&=~(3<<8); //PA4
+    ra&=~(3<<6); //PA3
+    PUT32(GPIOABASE+0x0C,ra);
+
+    spi_clk(0);
+    spi_data(0);
+    spi_cs(0);
+    spi_cs(1);
+
+    
+    spi_command(0x21); //extended commands
+//    spi_command(0xB0); //vop less contrast
+    spi_command(0xBF); //vop more contrast
+    spi_command(0x04); //temp coef
+    spi_command(0x14); //bias mode 1:48
+    spi_command(0x20); //extended off
+    spi_command(0x0C); //display on
+
+//experiment 1
+if(1)
+{
+    spi_command(0x80); //column
+    spi_command(0x40); //row
+    for(ra=0;ra<504;ra++) spi_data(ra);
+}
+
+    
+    return(0);
+}
