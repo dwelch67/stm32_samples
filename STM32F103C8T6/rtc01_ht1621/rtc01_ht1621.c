6957cff5c6a91b0beccde5a59ab9e328ecbac0a5
root <dwelch@dwelch.com>
2016-10-21 14:19:17 -0400

    rtc wip

--- STM32F103C8T6/rtc01_ht1621/rtc01_ht1621.c
+++ STM32F103C8T6/rtc01_ht1621/rtc01_ht1621.c
@@ -9,6 +9,14 @@ unsigned int GET32 ( unsigned int );
 void dummy ( unsigned int );
 void ASMDELAY ( unsigned int );
 
+#define USART1_BASE 0x40013800
+#define USART1_SR   (USART1_BASE+0x00)
+#define USART1_DR   (USART1_BASE+0x04)
+#define USART1_BRR  (USART1_BASE+0x08)
+#define USART1_CR1  (USART1_BASE+0x0C)
+#define USART1_CR2  (USART1_BASE+0x10)
+#define USART1_CR3  (USART1_BASE+0x14)
+#define USART1_GTPR (USART1_BASE+0x18)
 
 #define GPIOABASE 0x40010800
 #define GPIOA_BASE 0x40010800
@@ -55,6 +63,73 @@ static void clock_init ( void )
     PUT32(RCC_CFGR,ra);
     //wait for it
     while(1) if((GET32(RCC_CFGR)&0xF)==0x5) break;
+}
+
+static void uart_init ( void )
+{
+    //assuming 8MHz clock, 115200 8N1
+    unsigned int ra;
+
+    ra=GET32(RCC_APB2ENR);
+    ra|=1<<2;   //GPIOA
+    ra|=1<<14;  //USART1
+    PUT32(RCC_APB2ENR,ra);
+
+    //pa9 TX  alternate function output push-pull
+    //pa10 RX configure as input floating
+    ra=GET32(GPIOA_CRH);
+    ra&=~(0xFF0);
+    ra|=0x490;
+    PUT32(GPIOA_CRH,ra);
+
+    PUT32(USART1_CR1,0x2000);
+    PUT32(USART1_CR2,0x0000);
+    PUT32(USART1_CR3,0x0000);
+    //8000000/16 = 500000
+    //500000/115200 = 4.34
+    //4 and 5/16 = 4.3125
+    //4.3125 * 16 * 115200 = 7948800
+    PUT32(USART1_BRR,0x0045);
+    PUT32(USART1_CR1,0x200C);
+}
+//static unsigned int uart_getc ( void )
+//{
+    //while(1)
+    //{
+        //if(GET32(USART1_SR)&0x20) break;
+    //}
+    //return(GET32(USART1_DR)&0xFF);
+//}
+static void uart_putc ( unsigned int c )
+{
+    while(1)
+    {
+        if(GET32(USART1_SR)&0x80) break;
+    }
+    PUT32(USART1_DR,c);
+}
+static void hexstrings ( unsigned int d )
+{
+    //unsigned int ra;
+    unsigned int rb;
+    unsigned int rc;
+
+    rb=32;
+    while(1)
+    {
+        rb-=4;
+        rc=(d>>rb)&0xF;
+        if(rc>9) rc+=0x37; else rc+=0x30;
+        uart_putc(rc);
+        if(rb==0) break;
+    }
+    uart_putc(0x20);
+}
+static void hexstring ( unsigned int d )
+{
+    hexstrings(d);
+    uart_putc(0x0D);
+    uart_putc(0x0A);
 }
 
 
@@ -304,6 +379,9 @@ int notmain ( void )
     
     clock_init(); //still 8mhz just more accurate.
 
+    uart_init();
+    hexstring(0x12345678);
+
     rtc_init();
     ht_init();
 
@@ -312,8 +390,8 @@ int notmain ( void )
     last_time[2]=0;
     last_time[3]=0;
 
-    ht=0;ho=9;
-    mt=2;mo=4;
+    ht=0;ho=2;
+    mt=1;mo=6;
     st=0;so=0;
 
     show_clock();
@@ -367,6 +445,8 @@ int notmain ( void )
             }
             rb=ra;
             show_clock();
+            hexstrings(rb);
+            hexstring((ht<<20)|(ho<<16)|(mt<<12)|(mo<<8)|(st<<4)|(so<<0));
         }
     }
 
