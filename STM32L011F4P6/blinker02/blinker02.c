a76987f0bee345f94a72dc20506066f251dd961d
root <dwelch@dwelch.com>
2016-12-14 00:49:20 -0500

    work on STM32L0x1

--- STM32L011F4P6/blinker02/blinker02.c
+++ STM32L011F4P6/blinker02/blinker02.c
@@ -8,31 +8,22 @@ void dummy ( unsigned int );
 
 #define RCCBASE 0x40021000
 
+#define FLASHBASE 0x40022000
+
 int notmain ( void )
 {
     unsigned int ra;
     unsigned int rx;
 
     //SLOOOOW...
-    ra=GET32(RCCBASE+0x04);
-    ra&=~(7<<13);
-    PUT32(RCCBASE+0x04,ra);
+    //ra=GET32(RCCBASE+0x04);
+    //ra&=~(7<<13);
+    //PUT32(RCCBASE+0x04,ra);
 
-    ra=GET32(RCCBASE+0x2C);
-    ra|=1<<0; //enable port a
-    PUT32(RCCBASE+0x2C,ra);
-    //moder
-    ra=GET32(GPIOABASE+0x00);
-    ra&=~(3<<8); //PA4
-    ra&=~(3<<10); //PA5
-    ra|=1<<8; //PA5
-    ra|=1<<10; //PA5
-    PUT32(GPIOABASE+0x00,ra);
-    //OTYPER
-    ra=GET32(GPIOABASE+0x04);
-    ra&=~(1<<4); //PA4
-    ra&=~(1<<5); //PA5
-    PUT32(GPIOABASE+0x04,ra);
+    //shut down flash
+    PUT32(FLASHBASE+0x08,0x04152637);
+    PUT32(FLASHBASE+0x08,0xFAFBFCFD);
+    PUT32(FLASHBASE+0x00,0x00000018);
 
     ra=GET32(RCCBASE+0x2C);
     ra|=1<<1; //enable port b
@@ -52,19 +43,19 @@ int notmain ( void )
 
         PUT32(GPIOBBASE+0x18, (1<<(1+16)) );
 
-        PUT32(GPIOABASE+0x18, (1<<(4+ 0)) | (1<<(5+16)) );
+        //PUT32(GPIOABASE+0x18, (1<<(4+ 0)) | (1<<(5+16)) );
         for(ra=0;ra<10;ra++) dummy(ra);
-
-        PUT32(GPIOBBASE+0x18, (1<<(1+0)) );
-
-        PUT32(GPIOABASE+0x18, (1<<(4+16)) | (1<<(5+16)) );
-        for(ra=0;ra<4000;ra++) dummy(ra);
 
         //PUT32(GPIOBBASE+0x18, (1<<(1+0)) );
 
-        PUT32(GPIOABASE+0x18, (1<<(4+16)) | (1<<(5+ 0)) );
+        //PUT32(GPIOABASE+0x18, (1<<(4+16)) | (1<<(5+16)) );
+        for(ra=0;ra<4000;ra++) dummy(ra);
+
+        PUT32(GPIOBBASE+0x18, (1<<(1+0)) );
+
+        //PUT32(GPIOABASE+0x18, (1<<(4+16)) | (1<<(5+ 0)) );
         for(ra=0;ra<10;ra++) dummy(ra);
-        PUT32(GPIOABASE+0x18, (1<<(4+16)) | (1<<(5+16)) );
+        //PUT32(GPIOABASE+0x18, (1<<(4+16)) | (1<<(5+16)) );
         for(ra=0;ra<4000;ra++) dummy(ra);
     }
 
