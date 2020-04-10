7b2bca550353d35a651e553e2203f25a94da7af1
dwelch <dwelch@dwelch.com>
2020-04-09 20:47:59 -0400

    wip

--- STM32F031F4P6/fun/blinker20/flash.s
+++ STM32F031F4P6/fun/blinker20/flash.s
@@ -1,13 +1,11 @@
 
-;@-----------------------
 .cpu cortex-m0
 .thumb
-;@-----------------------
 
 .thumb_func
 .global _start
 _start:
-stacktop: .word 0x20000800
+.word 0x20000800
 .word reset
 .word hang
 .word hang
@@ -30,31 +28,26 @@ reset:
     b hang
 .thumb_func
 hang:   b .
-;@-----------------------
-.align
-;@-----------------------
+
 .thumb_func
 .globl PUT16
 PUT16:
     strh r1,[r0]
     bx lr
-;@-----------------------
+
 .thumb_func
 .globl PUT32
 PUT32:
     str r1,[r0]
     bx lr
-;@-----------------------
+
 .thumb_func
 .globl GET32
 GET32:
     ldr r0,[r0]
     bx lr
-;@-----------------------
+
 .thumb_func
 .globl dummy
 dummy:
     bx lr
-;@-----------------------
-.end
-;@-----------------------
